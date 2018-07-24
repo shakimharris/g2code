cb93863a9c5dce284dcb8fdcf227fe3404634ea0
amenraprod <amenraprod@gmail.com>
2018-07-24 18:59:15 -0400

    removed logo

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Trivia Nights</h1>
         </header>
         <p className="App-intro">
-          
+
         </p>
       </div>
     );
